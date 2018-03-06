7e31e63d867d07176bf566183614d277862c0af6
Sajil Awale <sajilawale@gmail.com>
2018-03-06 12:57:01 +0545

    added oval shape

--- main.cpp
+++ main.cpp
@@ -3,12 +3,56 @@
 #include <SFML/Graphics.hpp>
 using namespace std;
 
+class EllipseShape : public sf::Shape
+{
+public :
+
+    explicit EllipseShape(const sf::Vector2f& radius = sf::Vector2f(0, 0)) :
+    m_radius(radius)
+    {
+        update();
+    }
+
+    void setRadius(const sf::Vector2f& radius)
+    {
+        m_radius = radius;
+        update();
+    }
+
+    const sf::Vector2f& getRadius() const
+    {
+        return m_radius;
+    }
+
+    virtual unsigned int getPointCount() const
+    {
+        return 50; // fixed, but could be an attribute of the class if needed
+    }
+
+    virtual sf::Vector2f getPoint(unsigned int index) const
+    {
+        static const float pi = 3.141592654f;
+
+        float angle = index * 2 * pi / getPointCount() - pi / 2;
+        float x = std::cos(angle) * m_radius.x;
+        float y = std::sin(angle) * m_radius.y;
+
+        return sf::Vector2f(m_radius.x + x, m_radius.y + y);
+    }
+
+private :
+
+    sf::Vector2f m_radius;
+};
+
 class Process : public sf::Drawable
 {
 private:
+    sf::Vector2f Padding = sf::Vector2f(20,10);
     sf::Vector2f Position = sf::Vector2f(0,0); //initial
     sf::Vector2f Scale = sf::Vector2f(1,1);
     sf::Color text_outline_color = sf::Color::Black;
+    sf::String str  = "Text_Here";
 
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
         {
@@ -30,10 +74,11 @@ public:
         //cout << "rect: " << r.getSize().x << " , " << r.getSize().y << endl;
         r.setSize(sf::Vector2f(150, 50)); //hve to update on  setstring
         r.setOrigin(r.getGlobalBounds().width/2, r.getGlobalBounds().height/2);
+        cout << "rect: before: " << r.getGlobalBounds().width << " , " << r.getGlobalBounds().height << endl;
         r.setScale(Scale);
         //cout << "rect: " << r.getSize().x << " , " << r.getSize().y << endl;
         //r.setOrigin(r.getGlobalBounds().width/2, r.getGlobalBounds().height/2);
-        cout << "rect: " << r.getGlobalBounds().width << " , " << r.getGlobalBounds().height << endl;
+        cout << "rect: aftr: " << r.getGlobalBounds().width << " , " << r.getGlobalBounds().height << endl;
         r.setPosition(Position);               //mouse
         r.setOutlineThickness(4/(Scale.x+Scale.y));
         r.setOutlineColor(text_outline_color);    //depends on selection state
@@ -49,7 +94,7 @@ public:
 
         text.setFont(font); // font is a sf::Font
 
-        text.setString("Hello world");      // have to be user selectable
+        text.setString(str);      // have to be user selectable
 
         text.setCharacterSize(14*pow((Scale.x+Scale.y)/2, 0.7)); // in pixels, not points! // depends on scale
         text.setOrigin(text.getGlobalBounds().width/2, text.getGlobalBounds().height/2+3*Scale.y);
@@ -60,21 +105,38 @@ public:
         //text.setStyle(sf::Text::Bold | sf::Text::Underlined);
 
     }
+    void setString(const sf::String &s)
+    {
+        str = s;
+        //text.setOrigin(text.getGlobalBounds().width/2, text.getGlobalBounds().height/2+3*Scale.y);
+        text.setString(str);
 
+        sf::Vector2f rsize(2*Padding.x + text.getGlobalBounds().width , 2*Padding.y + text.getGlobalBounds().height);
+        r.setOrigin(r.getGlobalBounds().width/2, r.getGlobalBounds().height/2);
+        r.setSize(sf::Vector2f(rsize));
+        r.setOrigin(r.getGlobalBounds().width/2, r.getGlobalBounds().height/2);
+
+//
+        text.setOrigin(text.getGlobalBounds().width/2, text.getGlobalBounds().height/2+3*Scale.y);
+
+
+    }
     void setPosition(const sf::Vector2f &pos)
     {
          Position = pos;
          r.setPosition(Position);
          text.setPosition(Position);
     }
-    void setScale(const sf::Vector2f &sc)
-    {
-        Scale = sc;
-        r.setScale(Scale);
-        r.setOutlineThickness(4/(Scale.x+Scale.y));
-        text.setCharacterSize(14*pow((Scale.x+Scale.y)/2, 0.7));
-        text.setOrigin(text.getGlobalBounds().width/2, text.getGlobalBounds().height/2+3*Scale.y);
-    }
+//    void setScale(const sf::Vector2f &sc)
+//    {
+//        Scale = sc;
+//        r.setOrigin(r.getGlobalBounds().width/2, r.getGlobalBounds().height/2);
+//        r.setScale(Scale);
+//        r.setOutlineThickness(4/(Scale.x+Scale.y));
+//        r.setOrigin(r.getGlobalBounds().width/2, r.getGlobalBounds().height/2);
+//        text.setCharacterSize(14*pow((Scale.x+Scale.y)/2, 0.7));
+//        text.setOrigin(text.getGlobalBounds().width/2, text.getGlobalBounds().height/2+3*Scale.y);
+//    }
     void setColor(const sf::Color color)
     {
         text_outline_color = color;
@@ -82,6 +144,101 @@ public:
         text.setFillColor(text_outline_color);
     }
 };
+
+
+class Terminator : public sf::Drawable
+{
+private:
+    sf::Vector2f Padding = sf::Vector2f(30,10);
+    sf::Vector2f Position = sf::Vector2f(0,0); //initial
+    sf::Vector2f Scale = sf::Vector2f(1,1);
+    sf::Color text_outline_color = sf::Color::Black;
+    sf::String str  = "Text_Here";
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+        {
+            // You can draw other high-level objects
+            target.draw(e, states);
+            target.draw(text, states);
+        }
+
+    void setOrigin()
+    {
+        e.setOrigin(e.getGlobalBounds().width/2, e.getGlobalBounds().height/2);
+        text.setOrigin(text.getGlobalBounds().width/2, text.getGlobalBounds().height/2+5*Scale.y);
+    }
+
+public:
+
+    EllipseShape e;
+    sf::Font font;
+    sf::Text text;
+
+    Terminator()
+    {
+        e.setRadius(sf::Vector2f(70,50));
+        setOrigin();
+        e.setScale(Scale);
+        setOrigin();
+        e.setPosition(Position);
+        e.setOutlineThickness(4/(Scale.x+Scale.y));
+        e.setOutlineColor(text_outline_color);
+        e.setFillColor(sf::Color::White);
+
+
+        if (!font.loadFromFile("Roboto-Regular.ttf"))
+            std::cout << "Error loading font" << std::endl;
+
+        text.setFont(font);
+
+        setString(str);
+
+        text.setCharacterSize(14*pow((Scale.x+Scale.y)/2, 0.7)); // in pixels, not points! // depends on scale
+        setOrigin();
+
+        text.setFillColor(text_outline_color);
+        text.setPosition(Position);
+        // set the text style
+        //text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+    }
+
+    void setString(const sf::String &s)
+    {
+        str = s;
+        text.setString(str);
+
+        sf::Vector2f rsize(Padding.x + text.getGlobalBounds().width/2 , Padding.y + text.getGlobalBounds().height/2);
+        setOrigin();
+
+        e.setRadius(rsize);
+        setOrigin();
+    }
+
+    void setPosition(const sf::Vector2f &pos)
+    {
+         Position = pos;
+         e.setPosition(Position);
+         text.setPosition(Position);
+    }
+////    void setScale(const sf::Vector2f &sc)
+////    {
+////        Scale = sc;
+////        r.setOrigin(r.getGlobalBounds().width/2, r.getGlobalBounds().height/2);
+////        r.setScale(Scale);
+////        r.setOutlineThickness(4/(Scale.x+Scale.y));
+////        r.setOrigin(r.getGlobalBounds().width/2, r.getGlobalBounds().height/2);
+////        text.setCharacterSize(14*pow((Scale.x+Scale.y)/2, 0.7));
+////        text.setOrigin(text.getGlobalBounds().width/2, text.getGlobalBounds().height/2+3*Scale.y);
+////    }
+    void setColor(const sf::Color color)
+    {
+        text_outline_color = color;
+        e.setOutlineColor(text_outline_color);
+        text.setFillColor(text_outline_color);
+    }
+};
+
 
 
 int main()
@@ -93,8 +250,19 @@ int main()
     Process shape;
     cout << "width: " << shape.text.getGlobalBounds().width << " height: " << shape.text.getGlobalBounds().height << endl;
     //shape.setColor(sf::Color::Red);
-    shape.setPosition(sf::Vector2f(500,400));
-    shape.setScale(sf::Vector2f(2,2));
+    //shape.setPosition(sf::Vector2f(500,400));
+    //shape.setScale(sf::Vector2f(2,2));
+    //shape.setPosition(sf::Vector2f(500,400));
+    shape.setString("My name is Sajil Awale");
+    shape.setPosition(sf::Vector2f(400,400));
+
+    Terminator shape2;
+    //shape2.setColor(sf::Color::Red);
+    shape2.setPosition(sf::Vector2f(500,400));
+    //shape2.setScale(sf::Vector2f(2,2));
+    shape2.setPosition(sf::Vector2f(200,300));
+    shape2.setString("Start");
+
     while (window.isOpen())
     {
         //window.clear(sf::Color::White);
@@ -107,6 +275,7 @@ int main()
 
         window.clear(sf::Color::White);
         window.draw(shape);
+        window.draw(shape2);
         window.display();
     }
 
